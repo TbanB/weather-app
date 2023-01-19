90c75571cde19ba300dcce1047d1affe7b648307
Esteban Andres Bennett Burgos <esteban@Estebans-MacBook-Pro.local>
2023-01-20 00:09:24 +0100

    implemented languages

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
+  selectedLang: string = 'EN';
+
+  constructor(private translate: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.selectedLang = this.translate.currentLang;
+  }
+
+  onClickLang(): void {
+    this.selectedLang = this.selectedLang === 'es' ? 'en' : 'es';
+    this.translate.use(this.selectedLang);
+  }
 }
